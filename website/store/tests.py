52fa4930aac96ea75fa6a9916cb6fbba55149d60
Aaron <KyrumX@RGs-iMac.local>
2018-02-20 10:23:56 +0100

    Added tests

--- website/store/tests.py
+++ website/store/tests.py
@@ -1,3 +1,35 @@
+import unittest
+
 from django.test import TestCase
 
 # Create your tests here.
+from store.collections.tools import TransBool
+
+
+class ToolsTests(unittest.TestCase):
+    #Method naming: MethodName_Scenario_ExpectedBehavior()...
+    #Scenarios: all results/execution paths
+
+    def test_TransBool_BoolIsTrue_ReturnsJa(self):
+        #Arrange
+        testinput = True
+
+        #Act
+        result = TransBool(testinput)
+
+        #Assert
+        self.assertEqual("Ja", result)
+
+    def test_TransBool_BoolIsFalse_ReturnsNee(self):
+        testinput = False
+
+        result = TransBool(testinput)
+
+        self.assertEqual("Nee", result)
+
+    def test_TransBool_OtherTypeIsGiven_ReturnsNee(self):
+        testinput = "A string"
+
+        result = TransBool(testinput)
+
+        self.assertEqual("Nee", result)
